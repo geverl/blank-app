3865d4c0fbe4fb286959a9b951206838ed96236a
geverl <3144213+geverl@users.noreply.github.com>
2024-08-25 18:11:24 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("🎈 My first Streamlit steps")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
