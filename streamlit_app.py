275cf5b5200b5314bf50c5e4ed52e7f296706f1d
geverl <3144213+geverl@users.noreply.github.com>
2024-09-11 17:21:25 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,3 +86,11 @@ for i in range(100):
   latest_iteration.text(f'Iteration {i+1}')
   bar.progress(i + 1)
   time.sleep(0.1)
+
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
