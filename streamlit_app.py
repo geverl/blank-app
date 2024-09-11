408779f6f0f83ac078f942a482ea8ffdf01aedb2
geverl <3144213+geverl@users.noreply.github.com>
2024-09-11 17:23:58 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,3 +94,11 @@ st.session_state.counter += 1
 
 st.header(f"This page has run {st.session_state.counter} times.")
 st.button("Run it again")
+
+if "df" not in st.session_state:
+    st.session_state.df = pd.DataFrame(np.random.randn(20, 2), columns=["x", "y"])
+
+st.header("Choose a datapoint color")
+color = st.color_picker("Color", "#FF0000")
+st.divider()
+st.scatter_chart(st.session_state.df, x="x", y="y", color=color)
