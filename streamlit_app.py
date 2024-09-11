788c135dde7ec2e885cd37caf4d4e6886cef5483
geverl <3144213+geverl@users.noreply.github.com>
2024-09-11 13:17:22 +0200

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("🎈 My first Streamlit steps")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-import pandas as pd
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40],
@@ -14,3 +16,11 @@ df = pd.DataFrame({
 df
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
+
+
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
